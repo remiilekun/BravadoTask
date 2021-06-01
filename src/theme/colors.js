ce5a45128eca13f08384f9664f50ff6ee81ec777
Remilekun Salami <remilekun.salami@gmail.com>
2021-06-01 18:36:34 +0100

    feat: theme setup

---
+++ src/theme/colors.js
@@ -0,0 +1,11 @@
+export const colors = {
+  background: '#E7E7E7',
+  black: '#000000',
+  white: '#ffffff',
+  yellow: '#FFFA86',
+  grey: {
+    50: '#C4C4C4',
+    100: '#B3B3B3',
+    200: '#4D4D4D',
+  },
+};
